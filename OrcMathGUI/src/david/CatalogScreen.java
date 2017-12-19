2065cc63218ed18df78ba23be286098f975f1696
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-12-19 11:40:52 -0500

    buk

---
+++ OrcMathGUI/src/david/CatalogScreen.java
@@ -0,0 +1,25 @@
+package david;
+
+import java.util.List;
+
+import guiTeacher.components.TextField;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class CatalogScreen extends FullFunctionScreen {
+
+	private TextField descriptionField;
+	private TextField bookField;
+	
+	public CatalogScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		descriptionField = new TextField(40, 40, 200, 30, "Why Em Sea Ey", "Whats the other string?");
+		viewObjects.add(descriptionField);
+	}
+
+}
