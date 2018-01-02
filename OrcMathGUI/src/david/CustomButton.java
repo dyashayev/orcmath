91e83ccec22a4e29e9ff3753bc9469b70d52da7a
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-02 11:30:18 -0500

    fix color later

---
+++ OrcMathGUI/src/david/CustomButton.java
@@ -0,0 +1,40 @@
+package david;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class CustomButton extends Button{
+
+	private String s1 = "yes";
+	private String s2 = "no";
+	private Color ORANGE;
+
+	public CustomButton(int x, int y) {
+		super(x, y, 100, 50, "", null);
+	}
+	
+	public static void main(String[] arg) {
+		
+	}
+	public void drawButton(Graphics2D g, boolean hover){
+		g.setColor(Color.BLACK);
+		g.drawString(s1,100,100);
+		g.drawString(s2,20,20);
+		g.fillRect(0, 0, 50, 100);
+	}
+
+	void updateString1(String string) {
+		s1 = string;
+	}
+
+	void updateString2(String string) {
+		s2 = string;
+	}
+
+	void setIconColor(Color color) {
+		Color = color;
+	}
+}
